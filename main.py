46925097bef112d68ac5a38bcc1ab1eb0d802258
MUSUMBA ISAAC <isaacmusumba2023@gmail.com>
2026-05-15 00:52:06 +0300

    mm
    m

--- main.py
+++ main.py
@@ -17,6 +17,11 @@ st.set_page_config(
     initial_sidebar_state="expanded",
     menu_items={}
 )
+def local_css(file_name):
+    if os.path.exists(file_name):
+        with open(file_name) as f:
+            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+local_css("style.css")
 st.markdown(
         """
         <style>
