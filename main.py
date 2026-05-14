608b14b4f5fde15f12e43516a2d1ea78bbd5c269
MUSUMBA ISAAC <isaacmusumba2023@gmail.com>
2026-05-15 00:54:37 +0300

    mt
    mt

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from streamlit_option_menu import option_menu
 import pandas as pd
 from datetime import datetime, timedelta,date
 import plotly.express as px
+import os
 
 
 max_date = datetime.today().date()
@@ -22,6 +23,7 @@ def local_css(file_name):
         with open(file_name) as f:
             st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 local_css("style.css")
+
 st.markdown(
         """
         <style>
